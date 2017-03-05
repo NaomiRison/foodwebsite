1f52a9e69ee3134108dd9826e8049d910244c9c2
NaomiRison <nr.rison@gmail.com>
2017-03-05 16:51:34 -0500

    fixed navbar

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -2,3 +2,26 @@
 	// $("h1").on("click", function(){
 	// 	$(this).css("background-color","#ff0000");
 	// });
+/*
+
+
+*/
+
+
+ $(".js-features").waypoint(function(direction){
+ 	//  notice how file has waypoints
+ 	//and name of method is waypoint
+ 	/*
+        argument diecton determines if user scrolls
+        up or down. decsiion based on parameter, direction
+
+ 	*/
+ 	if(direction=="down"){
+    // sticky nav appears
+    $("nav").addClass("sticky");
+ 	}else{
+ 	  // user scrolls up remove class sticky
+ 	  $("nav").removeClass("sticky");
+
+ 	}
+ });
